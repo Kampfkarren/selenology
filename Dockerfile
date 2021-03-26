9aca62461de20339408b7cf7d7ee5005084e4dc5
Kampfkarren <boynedmaster@gmail.com>
2021-03-25 22:35:46 -0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM rust:buster
+
+WORKDIR /usr/src/selenology
+
+# I only barely get why I need to do this, something about Cargo reinstalling dependencies
+# every time the src/ changes in Docker because it doesn't support a dependencies only
+# configuration.
+COPY Cargo.toml Cargo.toml
+COPY Cargo.lock Cargo.lock
+RUN mkdir src/
+RUN echo "fn main() { eprintln!(\"You shouldn't be able to see this!\"); }" > src/main.rs
+RUN cargo build --release
+RUN rm -f target/release/deps/selenology*
+COPY . .
+RUN cargo build --release
+RUN cargo install --path .
+
+RUN apt install git=1:2.20.1-2+deb10u3
+
+RUN mkdir -p /usr/bin/selene
+RUN mkdir -p /usr/local/repos
+
+ENTRYPOINT [ "./entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+echo "selenology"
+
+echo "cloning current branch"
+git clone $INPUT_CURRENT_SELENE_REPOSITORY selene-current
+cd selene-current
+
+install() {
+	mkdir -p /usr/bin/$1
+	cargo install --path selene --root /usr/bin/$1
+	mv /usr/bin/$1/bin/selene /usr/bin/selene/$1
+}
+
+echo "building current branch"
+git checkout $INPUT_CURRENT_SELENE_CHECKOUT
+install selene-current &
+
+echo "cloning new branch"
+cd ..
+git clone $INPUT_NEW_SELENE_REPOSITORY selene-new
+
+echo "building new branch"
+cd selene-new
+git checkout $INPUT_NEW_SELENE_CHECKOUT
+install selene-new &
+
+wait
+
+OUTPUT=$(SELENE_NEW=/usr/bin/selene/selene-new \
+	SELENE_OLD=/usr/bin/selene/selene-current \
+	CLONE_DIRECTORY=/usr/local/repos \
+	selenology)
+OUTPUT="${OUTPUT//'%'/'%25'}"
+OUTPUT="${OUTPUT//$'\n'/'%0A'}"
+OUTPUT="${OUTPUT//$'\r'/'%0D'}"
+echo "::set-output name=output::$(echo "$OUTPUT")"
