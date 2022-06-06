0b60db4e29262f7f5f3d0be38fcc1dcc0eb01c23
Kampfkarren <boynedmaster@gmail.com>
2022-06-05 17:30:37 -0700

    Rewrite in JS

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 echo "selenology"
 
+echo "installing dependencies"
+npm i
+
 echo "cloning current branch"
 git clone $CURRENT_SELENE_REPOSITORY selene-current
 cd selene-current
@@ -31,7 +34,7 @@ wait
 OUTPUT=$(SELENE_NEW=/usr/bin/selene/selene-new \
 	SELENE_OLD=/usr/bin/selene/selene-current \
 	CLONE_DIRECTORY=/usr/local/repos \
-	selenology)
+	node ./selenology.js)
 OUTPUT="${OUTPUT//'%'/'%25'}"
 OUTPUT="${OUTPUT//$'\n'/'%0A'}"
 OUTPUT="${OUTPUT//$'\r'/'%0D'}"
---
+++ selonology.js
@@ -0,0 +1,124 @@
+const { exec: execChildProcess } = require("child_process");
+const diff = require("diff");
+const escape = require("escape-html");
+const fs = require("fs/promises");
+const path = require("path");
+const process = require("process");
+
+const repos = require("./repos.json");
+
+const write = process.stdout.write;
+
+const exec = (command, options) => {
+  return new Promise((resolve, reject) => {
+    execChildProcess(command, options, (error, stdout) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+};
+
+const process = (text) => escape(text).replace(/\n/g, "<br />");
+
+const renderDiff = (id, diff) => {
+  let rendered = `<div><h2>${id}</h2>`;
+
+  for (const diffPart of diff) {
+    if (diffPart.added) {
+      rendered += `<span style='background-color: #033a16; color: #aff5b4'>${process(
+        diffPart.value
+      )}</span>`;
+    } else if (diffPart.removed) {
+      rendered += `<span style='background-color: #67060c; color: #ffdcd7'>${process(
+        diffPart.value
+      )}</span>`;
+    } else {
+      rendered += process(diffPart.value);
+    }
+  }
+
+  rendered += "</div><hr />";
+  return rendered;
+};
+
+const main = async () => {
+  const { SELENE_OLD, SELENE_NEW, CLONE_DIRECTORY, DEBUG } = process.env;
+  if (!SELENE_OLD) {
+    throw new Error("SELENE_OLD not set");
+  }
+
+  if (!SELENE_NEW) {
+    throw new Error("SELENE_NEW not set");
+  }
+
+  if (!CLONE_DIRECTORY) {
+    throw new Error("CLONE_DIRECTORY not set");
+  }
+
+  const debug = (text) => {
+    if (DEBUG) {
+      console.debug(text);
+    }
+  };
+
+  write("<!DOCTYPE html>");
+  write("<html>");
+  write("<head>");
+  write("<meta charset='utf-8'>");
+  write("<style>body {{ font-family: Consolas, monospace; }}</style>");
+  write("</head>");
+  write("<body>");
+
+  const runOnRepo = async (repoName, repository) => {
+    debug(`scanning ${repoName}`);
+
+    const directory = path.join(CLONE_DIRECTORY, repoName);
+    await fs.mkdir(directory);
+
+    await exec("git init", { cwd: directory });
+    await exec(`git remote add origin ${repository.repo}`, { cwd: directory });
+    await exec(`git fetch --depth 1 origin ${repository.branch}`, {
+      cwd: directory,
+    });
+    await exec(`git checkout FETCH_HEAD`, { cwd: directory });
+
+    debug("running old selene");
+    const oldOutput = await exec(
+      `${SELENE_OLD} ${repository.args.join(" ")} --num-threads 1`,
+      {
+        cwd: directory,
+      }
+    );
+
+    const robloxStd = path.join(directory, "roblox.yml");
+    if (path.exists(robloxStd)) {
+      debug(`deleting ${robloxStd}`);
+      await fs.rm(robloxStd);
+    }
+
+    debug("running new selene");
+    const newOutput = await exec(
+      `${SELENE_NEW} ${repository.args.join(" ")} --num-threads 1`,
+      {
+        cwd: directory,
+      }
+    );
+
+    debug("checking difference");
+
+    const difference = diff.diffTrimmedLines(oldOutput, newOutput);
+    write(renderDiff(repoName, difference));
+  };
+
+  for (const [repoName, repo] of Object.entries(repos)) {
+    await runOnRepo(repoName, repo);
+  }
+};
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
